9d9086855aa1d10095cfb1ebea4fa9a88fbb891a
afcarrion <af.carrion@uniandes.edu.co>
2021-06-21 23:48:08 -0500

    Test

--- src/com/afcarrion/Main.java
+++ src/com/afcarrion/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        //prueba
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
